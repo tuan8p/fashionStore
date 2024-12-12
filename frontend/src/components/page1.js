0871c9940463d4190d2b5018eb6941142fca7c4d
duongnguyen1634 <thuyduong110804@gmail.com>
2024-12-12 22:07:49 +0700

    backend

--- frontend/src/components/page1.js
+++ frontend/src/components/page1.js
@@ -5,95 +5,148 @@ import dayjs from 'dayjs';
 
 const {Option} = Select;
 
+const useWindowSize = () => {
+  const [size, setSize] = useState({ width: window.innerWidth });
+
+  useEffect(() => {
+    const handleResize = () => setSize({ width: window.innerWidth });
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return size;
+};
 // Data mẫu
-const data = [
-    {
-      id: "DH20240121113078FG7",
-      accountId: "z2x8v5k3w1",
-      houseNumber: "56",
-      district: "Sơn Trà",
-      city: "Đà Nẵng",
-      paymentStatus: "Ebanking",
-      orderDate: "15/02/2024",
-      deliveryDate: "15/02/2024",
-      orderStatus: "Đang xử lý"
-    },
-    {
-      id: "DH202401211109FT",
-      accountId: "x7n4p7ef89",
-      houseNumber: "56",
-      district: "Sơn Trà",
-      city: "Đà Nẵng",
-      paymentStatus: "MoMo",
-      orderDate: "12/10/2024",
-      deliveryDate: "12/10/2024",
-      orderStatus: "Đang xử lý",
-    },
-];
+// const data = [
+//     {
+//       id: "DH20240121113078FG7",
+//       accountId: "z2x8v5k3w1",
+//       houseNumber: "56",
+//       district: "Sơn Trà",
+//       city: "Đà Nẵng",
+//       paymentStatus: "Ebanking",
+//       orderDate: "15/02/2024",
+//       deliveryDate: "15/02/2024",
+//       orderStatus: "Đang xử lý"
+//     },
+//     {
+//       id: "DH202401211109FT",
+//       accountId: "x7n4p7ef89",
+//       houseNumber: "56",
+//       district: "Sơn Trà",
+//       city: "Đà Nẵng",
+//       paymentStatus: "MoMo",
+//       orderDate: "12/10/2024",
+//       deliveryDate: "12/10/2024",
+//       orderStatus: "Đang xử lý",
+//     },
+// ];
 
 const Page1 = () => {
+  const { width } = useWindowSize();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/api/DonHang')
+        .then(response => response.json())
+        .then(data => setData(data))  // Cập nhật state data
+        .catch(error => console.error('Error fetching data:', error));
+  }, []);
     const columns = [
         {
           title: "Mã đơn hàng",
-          dataIndex: "id",
-          key: "id",
+          dataIndex: 'ma_don_hang',
+          key: 'ma_don_hang',
+        },
+        {
+          title: 'Mã giỏ hàng',
+          dataIndex: 'ma_gio_hang',
+          key: 'ma_gio_hang',
+          responsive: ['lg'],
         },
         {
           title: "Mã tài khoản",
-          dataIndex: "accountId",
-          key: "accountId",
+          dataIndex: "ma_tai_khoan",
+          key: "ma_tai_khoan",
+        },
+        {
+          title: "Mã thanh toán",
+          dataIndex: "ma_thanh_toan",
+          key: "ma_thanh_toan",
+          responsive: ['md'],
+        },
+        {
+          title: "Mã vận chuyển",
+          dataIndex: "ma_van_chuyen",
+          key: "ma_van_chuyen",
+          responsive: ['lg'],
+        },
+        {
+          title: "Mã khuyến mãi",
+          dataIndex: "ma_khuyen_mai",
+          key: "ma_khuyen_mai",
+          responsive: ['lg'],
         },
         {
           title: "Số nhà",
-          dataIndex: "houseNumber",
-          key: "houseNumber",
+          dataIndex: "so_nha",
+          key: "so_nha",
           responsive: ['md'],
         },
         {
           title: "Quận/Huyện",
-          dataIndex: "district",
-          key: "district",
+          dataIndex: "quan_huyen",
+          key: "quan_huyen",
           responsive: ['md'],
         },
         {
           title: "Tỉnh/Thành phố",
-          dataIndex: "city",
-          key: "city",
+          dataIndex: "thanh_pho",
+          key: "thanh_pho",
           responsive: ['md'],
         },
         {
           title: "Tình trạng thanh toán",
-          dataIndex: "paymentStatus",
-          key: "paymentStatus",
+          dataIndex: "tinh_trang_thanh_toan",
+          key: "tinh_trang_thanh_toan",
           render: (status) => {
             const color = 
-                status === "Ebanking" ? "green" : 
-                status === "MoMo" ? "orange" : 
-                status === "ATM" ? "blue" : 
-                "violet";
+            status === 'Đã thanh toán' ? 'green' : 
+            status === 'Đã hoàn tiền' ? 'red' : 'blue';
             return <Tag color={color}>{status}</Tag>;
           },
         },
         {
-          title: "Ngày đặt hàng",
-          dataIndex: "orderDate",
-          key: "orderDate",
+          title: 'Ngày đặt hàng',
+          dataIndex: 'ngay_dat_hang',
+          key: 'ngay_dat_hang',
+          render: (date) => {
+            return date ? dayjs(date).format('DD/MM/YYYY') : '';},
+          responsive: ['lg'],
+        },
+        {
+          title: 'Ngày nhận hàng',
+          dataIndex: 'ngay_nhan_hang',
+          key: 'ngay_nhan_hang',
+          render: (date) => {
+            return date ? dayjs(date).format('DD/MM/YYYY') : '';}, // Định dạng ngày
           responsive: ['lg'],
         },
         {
           title: "Thời gian xuất đơn",
-          dataIndex: "deliveryDate",
-          key: "deliveryDate",
+          dataIndex: "thoi_gian_xuat_hoa_don",
+          key: "thoi_gian_xuat_hoa_don",
+          render: (date) => {
+            return date ? dayjs(date).format('DD/MM/YYYY') : '';},
           responsive: ['lg'],
         },
         {
             title: 'Trạng thái đơn hàng',
-            dataIndex: 'orderStatus',
-            key: 'orderStatus',
+            dataIndex: 'trang_thai_don_hang',
+            key: 'trang_thai_don_hang',
             render: (text) => {
               let color = '';
               switch (text) {
-                case 'Đã giao':
+                case 'Đã giao hàng':
                   color = '#6226EF';
                   break;
                 case 'Đang xử lý':
@@ -102,6 +155,9 @@ const Page1 = () => {
                 case 'Đã hủy':
                   color = 'red';
                   break;
+                case 'Đang vận chuyển':
+                  color = 'green';
+                  break;
                 default:
                   color = 'gray';
               }
@@ -109,47 +165,35 @@ const Page1 = () => {
             },
         },
         {
-          title: "",
-          key: "actions",
+          title: '',
+          key: 'actions',
           render: (_, record) => (
-            <div>
-              <Button
-                type="primary"
-                shape="round"
-                icon={<EditOutlined />}
-                style={{ marginRight: 8}}
-                onClick={() => showModal('changeOrder', record)}
-              >
-                Thay đổi
-              </Button>
-              <Button
-                type="primary"
-                danger
-                shape="round"
-                icon={<DeleteOutlined />}
-                disabled={record.orderStatus !== "Đang xử lý"}
-                onClick={() => showModal('deleteOrder', record)}
-              >
-                Xóa
-              </Button>
+          <div>
+            <Button
+              type="primary"
+              shape="round"
+              icon={<EditOutlined />}
+              style={{ marginRight: 8 }}
+              onClick={() => showModal('changeOrder', record)}
+            >Thay đổi
+            </Button>
+            <Button
+            type="primary"
+            danger
+            shape="round"
+            icon={<DeleteOutlined />}
+            onClick={() => showModal('deleteOrder', record)}
+            >Xóa
+            </Button>
             </div>
-          ),
-        },
+        ),
+      },
     ];
     
     const [currentOrder, setCurrentOrder] = useState(null);
-    const useWindowSize = () => {
-      const [size, setSize] = useState({ width: window.innerWidth });
-    
-      useEffect(() => {
-        const handleResize = () => setSize({ width: window.innerWidth });
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-      }, []);
-    
-      return size;
-    };
-    const { width } = useWindowSize();
+    const [modalVisible, setModalVisible] = useState(null);
+    //const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
     //Hàm xử lý khi bấm "Lưu đơn hàng" trong modal
     const handleEditSave = (values) => {
       console.log('Thông tin đã lưu: ', values);
@@ -158,18 +202,17 @@ const Page1 = () => {
     
     // Hàm xử lý khi bấm "Xác nhận" trong modal
     const handleConfirm = () => {
-      console.log(`Đơn hàng ${currentOrder?.id} đã bị hủy!`);
-      setCurrentOrder(null);
+      console.log(`Đơn hàng ${currentOrder?.ma_don_hang} đã bị hủy!`);
       setModalVisible(false);
     };
-    const [modelVisible, setModalVisible] = useState(null);
-    const showModal = (id, order = null) => {
-      setModalVisible(id);
+    //const [modalType, setModalType] = useState(''); 
+    
+    const showModal = (modalType, order = null) => {
+      setModalVisible(modalType); // Chỉ gọi một lần để thay đổi trạng thái modal
       if (order) {
-        order.orderDate = dayjs(order.orderDate, 'DD/MM/YYYY'); // Định dạng gốc trong data
-        order.deliveryDate = dayjs(order.deliveryDate, 'DD/MM/YYYY');
+        order.ngay_dat_hang = dayjs(order.ngay_dat_hang).format('DD/MM/YYYY');  // Định dạng ngày nếu cần
+        order.ngay_nhan_hang = dayjs(order.ngay_nhan_hang).format('DD/MM/YYYY');
       }
-      setModalVisible(id);
       setCurrentOrder(order);
     };
   
@@ -202,15 +245,15 @@ const Page1 = () => {
       <Table
         dataSource={data}
         columns={columns}
-        //rowKey="id"
-        style={{ marginTop: '20px' }}
+        rowKey="ma_don_hang"
         pagination={{ pageSize: 10 }}
-        scroll={{ x: '100%' }}
+        style={{ marginTop: '20px' }}
+        scroll={{ x: true }}
       />
 
     <Modal
         title="Xác nhận hủy đơn hàng ?"
-        open = {modelVisible === 'deleteOrder'}
+        open = {modalVisible === 'deleteOrder'}
         onOk={handleConfirm}
         onCancel={handleCancel}
         okText="Xác nhận"
@@ -221,7 +264,7 @@ const Page1 = () => {
     </Modal>
     <Modal
         title="Điền thông tin đơn hàng"
-        open = {modelVisible === 'createOrder'}
+        open = {modalVisible === 'createOrder'}
         onCancel={handleCancel}
         footer={null}
         width={width > 768 ? 600 : "90%"}
@@ -231,39 +274,41 @@ const Page1 = () => {
           onFinish={handleEditSave}
           layout="vertical"
         >
-          <Form.Item label="Mã đơn hàng" name="id">
+          <Form.Item label="Mã đơn hàng" name="ma_don_hang">
             <Input disabled />
           </Form.Item>
-          <Form.Item label="Mã tài khoản" initialValue="accountId">
+          <Form.Item label="Mã tài khoản" name="ma_tai_khoan">
             <Input />
           </Form.Item>
-          <Form.Item label="Số nhà" initialValue="houseNumber">
+          <Form.Item label="Số nhà" name="so_nha">
             <Input />
           </Form.Item>
-          <Form.Item label="Quận/ Huyện" initialValue="district">
+          <Form.Item label="Quận/Huyện" name="quan_huyen">
             <Input />
           </Form.Item>
-          <Form.Item label="Tỉnh/ Thành phố" initialValue="city">
+          <Form.Item label="Tỉnh/ Thành phố" name="thanh_pho">
             <Input />
           </Form.Item>
-          <Form.Item label="Tình trạng thanh toán" initialValue="paymentStatus">
+          <Form.Item label="Tình trạng thanh toán" name="tinh_trang_thanh_toan">
             <Select>
-              <Option value="Ebanking">Ebanking</Option>
-              <Option value="MoMo">MoMo</Option>
-              <Option value="ATM">ATM</Option>
+              <Option value="Đã thanh toán">Đã thanh toán</Option>
+              <Option value="Đã hoàn tiền">Đã hoàn tiền</Option>
+              <Option value="Chưa thanh toán">Chưa thanh toán</Option>
             </Select>
           </Form.Item>
-          <Form.Item label="Ngày đặt hàng" initialValue="orderDate">
-            <DatePicker />
+          <Form.Item label="Ngày đặt hàng" name="ngay_dat_hang">
+            <DatePicker value={currentOrder?.ngay_dat_hang ? dayjs(currentOrder.ngay_dat_hang).format('DD/MM/YYYY') : ''}/>
           </Form.Item>
-          <Form.Item label="Thời gian xuất đơn" initialValue="deliveryDate">
-            <DatePicker />
+          <Form.Item label="Ngày nhận hàng" name="ngay_nhan_hang">
+            <DatePicker value={currentOrder?.ngay_nhan_hang ? dayjs(currentOrder.ngay_nhan_hang).format('DD/MM/YYYY') : '' }/>
           </Form.Item>
-          <Form.Item label="Trạng thái đơn hàng" initialValue="orderStatus">
+          <Form.Item label="Trạng thái đơn hàng" name="trang_thai_don_hang">
             <Select>
               <Option value="Đang xử lý">Đang xử lý</Option>
-              <Option value="Đã giao">Đã giao</Option>
+              <Option value="Đang vận chuyển">Đang vận chuyển</Option>
+              <Option value="Đã giao hàng">Đã giao hàng</Option>
               <Option value="Đã hủy">Đã hủy</Option>
+              <Option value="Đã hoàn trả">Đã hoàn trả</Option>
             </Select>
           </Form.Item>
 
@@ -277,7 +322,7 @@ const Page1 = () => {
 
     <Modal
         title="Chỉnh sửa đơn hàng"
-        open = {modelVisible === 'changeOrder'}
+        open = {modalVisible === 'changeOrder'}
         onCancel={handleCancel}
         footer={null}
         width={width > 768 ? 600 : "90%"}
@@ -287,40 +332,49 @@ const Page1 = () => {
           layout="vertical"
           initialValues={currentOrder || {}}
         >
-          <Form.Item label="Mã đơn hàng" name="id">
+          <Form.Item label="Mã đơn hàng" name="ma_don_hang">
             <Input disabled input="1"/>
           </Form.Item>
-          <Form.Item label="Mã tài khoản" name="accountId">
+          <Form.Item label="Mã tài khoản" name="ma_tai_khoan">
             <Input disabled/>
           </Form.Item>
-          <Form.Item label="Số nhà" name="houseNumber">
+          <Form.Item label="Số nhà" name="so_nha">
             <Input disabled/>
           </Form.Item>
-          <Form.Item label="Quận/ Huyện" name="district">
+          <Form.Item label="Quận/ Huyện" name="quan_huyen">
             <Input disabled/>
           </Form.Item>
-          <Form.Item label="Tỉnh/ Thành phố" name="city">
+          <Form.Item label="Tỉnh/ Thành phố" name="thanh_pho">
             <Input disabled/>
           </Form.Item>
-          <Form.Item label="Tình trạng thanh toán" name="paymentStatus">
+          <Form.Item label="Tình trạng thanh toán" name="tinh_trang_thanh_toan">
             <Select disabled>
-              <Option value="Ebanking">Ebanking</Option>
-              <Option value="MoMo">MoMo</Option>
-              <Option value="ATM">ATM</Option>
+            <Option value="Đã thanh toán">Đã thanh toán</Option>
+            <Option value="Đã hoàn tiền">Đã hoàn tiền</Option>
+            <Option value="Chưa thanh toán">Chưa thanh toán</Option>
             </Select>
           </Form.Item>
-          <Form.Item label="Ngày đặt hàng" name="orderDate">
-            <DatePicker disabled/>
+          <Form.Item label="Ngày đặt hàng" name="ngay_dat_hang">
+            <Input
+              disabled
+              value={currentOrder?.ngay_dat_hang ? dayjs(currentOrder.ngay_dat_hang).format('DD/MM/YYYY') : ''}
+            />
           </Form.Item>
-          <Form.Item label="Thời gian xuất đơn" name="deliveryDate">
-            <DatePicker disabled/>
+
+          <Form.Item label="Ngày nhận hàng" name="ngay_nhan_hang">
+            <Input
+              disabled
+              value={currentOrder?.ngay_nhan_hang ? dayjs(currentOrder.ngay_nhan_hang).format('DD/MM/YYYY') : ''}
+            />
           </Form.Item>
-          <Form.Item label="Trạng thái đơn hàng" name="orderStatus"
+          <Form.Item label="Trạng thái đơn hàng" name="trang_thai_don_hang"
             initialValue="Đang xử lý">
             <Select>
-              <Option value="Đang xử lý">Đang xử lý</Option>
-              <Option value="Đã giao">Đã giao</Option>
+            <Option value="Đang xử lý">Đang xử lý</Option>
+              <Option value="Đang vận chuyển">Đang vận chuyển</Option>
+              <Option value="Đã giao hàng">Đã giao hàng</Option>
               <Option value="Đã hủy">Đã hủy</Option>
+              <Option value="Đã hoàn trả">Đã hoàn trả</Option>
             </Select>
           </Form.Item>
 
